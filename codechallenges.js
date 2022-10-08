985cb5e860f7397362926e18979fb1c0189e10ea
Yasir Younas <yazyounas@gmail.com>
2022-10-08 14:50:02 +0100

    added more code challenges using for loops

--- codechallenges.js
+++ codechallenges.js
@@ -32,3 +32,37 @@ function reverseArray(arr) {
 const sentence = ['sense.','make', 'all', 'will', 'This'];
 
 console.log(reverseArray(sentence));
+
+// Write your code here:
+
+//array of strings and uses a for loop to print a greeting with each string in the array.
+const greetAliens = arr => {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+      console.log(`Oh powerful ${arr[i]}, we humans offer our unconditional surrender!`)
+      newArr.push(arr[i]);
+    }
+    return newArr;
+  }
+  
+  
+  
+  const aliens = ["Blorgous", "Glamyx", "Wegord", "SpaceKing"];
+  greetAliens(aliens)
+
+  //returns a new array with each string in the array prepended with 'baby '.
+  // Write your code here:
+const convertToBaby = arr => {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+     newArr.push('baby ' + arr[i])
+    }
+    
+    return newArr;
+}
+  
+  const animals = ['panda', 'turtle', 'giraffe', 'hippo', 'sloth', 'human'];
+  
+  console.log(convertToBaby(animals)) 
+  
+  
