4394bbd2e58df9478ba11e3cfec7145799e3fc6d
Yasir Younas <yazyounas@gmail.com>
2022-10-09 22:05:56 +0100

    added more code challenges

--- codechallenges.js
+++ codechallenges.js
@@ -65,4 +65,98 @@ const convertToBaby = arr => {
   
   console.log(convertToBaby(animals)) 
   
+  //Write a function acceptEverything() that takes in an array of strings and loops through each element in the array and grudgingly accepts each of them, by logging to the console in the following format: 'Ok, I guess I will eat some [element].
+  const veggies = ['broccoli', 'spinach', 'cauliflower', 'broccoflower'];
+
+const politelyDecline = (veg) => {
+      console.log('No ' + veg + ' please. I will have pizza with extra cheese.');
+}
+
+// Write your code here:
+const declineEverything = arr => {
+  arr.forEach(politelyDecline);
+}
+console.log(declineEverything(veggies));
+const acceptEverything = veg => {
+ veg.forEach(vege => {
+   console.log(`Ok, I guess I will eat some ${vege}.`)
+ });
+}
+console.log(acceptEverything(veggies));
+// returns an array with the square of each of the elements of that array.
+const numbers = [2, 7, 9, 171, 52, 33, 14]
+
+const toSquare = num => num * num
+
+// Write your code here:
+const squareNums = arr => arr.map(toSquare);
+
+console.log(squareNums(numbers));
+
+
+
+//argument array but with capitalized letters and an exclamation point appended to the end:
+
+// Write your code here:
+
+/*const shoutGreetings = arr => {
+  let greet = [];
+  for (let i = 0; i < arr.length; i++) {
+    greet.push(arr[i].toUpperCase() + '!');
+  }
+  return greet;
+}
+*/
+const shoutGreetings = word => word.map(words =>words.toUpperCase() + '!')
   
+
+
+/*const shoutGreetings = arr => arr.map(word => wordtoUpperCase() + '!');
+*/
+
+// Feel free to uncomment out the code below to test your function!
+
+const greetings = ['hello', 'hi', 'heya', 'oi', 'hey', 'yo'];
+
+console.log(shoutGreetings(greetings))
+// Should print [ 'HELLO!', 'HI!', 'HEYA!', 'OI!', 'HEY!', 'YO!' ]
+
+//returns that array with the years sorted in descending order.
+const sortYears = arr => arr.sort((year1, year2) => year2 - year1);
+
+
+
+
+
+
+// Feel free to uncomment the below code to test your function:
+
+const years = [1970, 1999, 1951, 1982, 1963, 2011, 2018, 1922]
+
+console.log(sortYears(years))
+// Should print [ 2018, 2011, 1999, 1982, 1970, 1963, 1951, 1922 ]
+
+//returns an array with the items that are present in both arrays.
+
+const justCoolStuff = (firstArray, secondArray) => firstArray.filter(item => secondArray.includes(item))
+
+/*
+// As a function declaration AND using named function w/ a loop:
+function justCoolStuff(firstArray, secondArray) {
+      function isInSecondArray(item){
+            for(let i = 0; i<secondArray.length; i++){
+                  if (secondArray[i] === item){
+                        return true
+                  }
+            }
+            return false 
+      }
+      return firstArray.filter(isInSecondArray)
+}
+*/
+
+const coolStuff = ['gameboys', 'skateboards', 'backwards hats', 'fruit-by-the-foot', 'pogs', 'my room', 'temporary tattoos'];
+
+const myStuff = [ 'rules', 'fruit-by-the-foot', 'wedgies', 'sweaters', 'skateboards', 'family-night', 'my room', 'braces', 'the information superhighway']; 
+
+console.log(justCoolStuff(myStuff, coolStuff))
